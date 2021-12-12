9d401c2c410fca3f13f63ddab4c4a68744bf2508
Dalvin <dalvin.josias@gmail.com>
2021-12-11 21:37:00 -0800

    learn vec

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -1,18 +1,18 @@
 // move_semantics1.rs
 // Make me compile! Execute `rustlings hint move_semantics1` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
 
-    let vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
     vec1.push(88);
+    vec1.push(50);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    println!("The vec sum is {}", add_vec(vec1));
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
@@ -23,4 +23,13 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     vec.push(66);
 
     vec
+}
+
+fn add_vec(vec: Vec<i32>) -> i32 {
+    let mut sum = 0;
+    for i in vec {
+        sum += i;
+    }
+
+    sum
 }
